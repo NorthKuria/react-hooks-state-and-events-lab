a8e104fbf1e72f2ea0985ec13e125bf466f83ceb
northkuria <kuri4martin@gmail.com>
2022-07-20 13:24:07 +0300

    filter lists

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from 'react'
 
 function Item({ name, category }) {
+  const [cart, setCart] = useState(false)
+
+  const cname = cart ? 'in-cart' : ''
+  const text = cart ? 'Remove From Cart' : 'Add to Cart'
+
+  const handleClick = () => {
+    setCart((prev) => !prev)
+  }
+
   return (
-    <li className="">
+    <li className={cname}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleClick}>
+        {text}
+      </button>
     </li>
-  );
+  )
 }
 
-export default Item;
+export default Item
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,21 @@
-import React from "react";
-import Item from "./Item";
+import React, { useState } from 'react'
+import Item from './Item'
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState('All')
+
+  const handleChange = (e) => {
+    setSelectedCategory(e.target.value)
+  }
+
+  const filteredItems = items.filter((item) => {
+    return item.category === selectedCategory
+  })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +23,12 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
-  );
+  )
 }
 
-export default ShoppingList;
+export default ShoppingList
